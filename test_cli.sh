87e5bf4f4d36282ddfdbfc7fb14560dc6b593d50
Jonathan Helmus <jjhelmus@gmail.com>
2019-01-17 17:07:22 -0600

    add test_cli.sh script

---
+++ test_cli.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+create_clobber --help
+extract_index_json --help
+find_latest --help
+find_outdated_packages_pypi --help
+gh --help
+prepare_batch_file --help
+push_feedstock_changes --help
+pypi --help
+replace_index_json --help
+reqs --help
+run_grimlock --help
+show_git_messages --help
+sync_cf --help
+update_recipe --help
