144084e02c151488fbaf7b7519cf9a75a2e07d76
Jonathan Helmus <jjhelmus@gmail.com>
2019-01-18 09:42:11 -0600

    load pkg version info from env var
    The find_latest command uses the file specified in the PKG_INFO_FILE
    environment variable to determine version information.  If this is not set the
    previous default is uses.
    
    Add the --no_header option to find_latest

--- conda_recipe_tools/cli/find_latest.py
+++ conda_recipe_tools/cli/find_latest.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 import argparse
+import os
 
 from conda_recipe_tools.find_version import find_latest_version
 from conda_recipe_tools.pkg_info import read_pkg_info
@@ -9,8 +10,15 @@ def parse_arguments():
     parser = argparse.ArgumentParser(
         description="Find the latest version of packages")
     parser.add_argument(
-        "--pkg_info", action='store', default='pkg_info.csv',
-        help='file containing specification on how to find package versions')
+        "--pkg_info", action='store', default=None,
+        help=(
+            'File containing specification on how to find package versions. '
+            'The default is to use the file specified by the PKG_INFO_FILE '
+            'environment variable. If this variable is not set, pkg_info.csv '
+            'in the current directy is used.'))
+    parser.add_argument(
+        "--no_header", action='store_true',
+        help='Do not print header line, helpful when appending to a file')
     parser.add_argument(
         'packages', nargs='*',
         help='packages to check, leave blank to check all packages')
@@ -19,12 +27,16 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
-    pkg_info = read_pkg_info(args.pkg_info)
+    pkg_info = args.pkg_info
+    if pkg_info is None:
+        pkg_info = os.environ.get('PKG_INFO_FILE', 'pkg_info.csv')
+    pkg_info = read_pkg_info(pkg_info)
     if len(args.packages):
         names_to_check = args.packages
     else:
         names_to_check = sorted(pkg_info.keys())
-    print('package_name,latest_version')
+    if not args.no_header:
+        print('package_name,latest_version')
     for name in names_to_check:
         latest_version = find_latest_version(name, pkg_info[name])
         print(f'{name},{latest_version}')
