7770f20919cdc02e71c6d94519060bf4910b9b44
Jonathan Helmus <jjhelmus@gmail.com>
2019-01-17 19:20:01 -0600

    use crt objects in push_feedstock_changes

--- conda_recipe_tools/cli/push_feedstock_changes.py
+++ conda_recipe_tools/cli/push_feedstock_changes.py
@@ -5,87 +5,13 @@
 import argparse
 import logging
 import os
-import subprocess
 import sys
-from subprocess import PIPE
 
-from conda_build.api import render
+from conda_recipe_tools.git import FeedStock, NotFeedstockRepo
+from conda_recipe_tools.recipe import CondaRecipe
+
 
 LOG_FORMAT = '%(asctime)s - %(levelname)s : %(message)s'
-
-
-class NotFeedstockRepo(Exception):
-    pass
-
-
-class GitRepo(object):
-
-    def __init__(self, path):
-        self._path = path
-
-    def _git(self, git_args, check=True):
-        args = ['git', '-C', self._path] + git_args
-        logging.debug('command: ' + ' '.join(args))
-        complete = subprocess.run(args, stdout=PIPE, stderr=PIPE)
-        logging.debug('returncode: ' + str(complete.returncode))
-        logging.debug('stdout: ' + complete.stdout.decode('utf-8'))
-        logging.debug('stderr: ' + complete.stderr.decode('utf-8'))
-        if check:
-            complete.check_returncode()
-        return complete
-
-    def checkout(self, branch='master'):
-        return self._git(['checkout', branch])
-
-    def fetch(self, remote='origin'):
-        return self._git(['fetch', remote])
-
-    def reset_hard(self, remote='origin', branch='master'):
-        return self._git(['reset', '--hard', '%s/%s' % (remote, branch)])
-
-    def rebase(self, remote, branch='master', check=True):
-        return self._git(['rebase', '%s/%s' % (remote, branch)], check)
-
-    def rebase_abort(self, check=True):
-        self._git(['rebase', '--abort'], check)
-
-    def push(self, remote='origin', local_branch='master',
-             remote_branch='master', force=False, check=True):
-        refspec = '{src}:{dst}'.format(src=local_branch, dst=remote_branch)
-        if force:
-            return self._git(['push', '--force', remote, refspec], check)
-        return self._git(['push', remote, refspec], check)
-
-    def branch(self, branch_name):
-        return self._git(['checkout', '-b', branch_name])
-
-    def branch_delete(self, branch_name):
-        return self._git(['branch', '-D', branch_name])
-
-    def ls_files_modified(self):
-        out = self._git(['ls-files', '-m'])
-        return out.stdout.decode('utf-8').split()
-
-
-class FeedStock(GitRepo):
-
-    def __init__(self, path, feedstock_name=None):
-        super(FeedStock, self).__init__(path)
-        if feedstock_name is None:
-            out = self._git(['config', '--get', 'remote.origin.url'])
-            origin_url = out.stdout.decode('utf-8').strip()
-            feedstock_name = origin_url.split('/')[-1]
-            if feedstock_name.endswith('.git'):
-                feedstock_name = feedstock_name[:-4]
-        if not feedstock_name.endswith('feedstock'):
-            raise NotFeedstockRepo('not a feedstock: %s' % (feedstock_name))
-        self._feedstock_name = feedstock_name
-
-    def add_remote(self, org, remote_name=None, check=True):
-        url = 'https://github.com/%s/%s' % (org, self._feedstock_name)
-        if remote_name is None:
-            remote_name = org
-        self._git(['remote', 'add', remote_name, url], check=check)
 
 
 def push_feedstock(feedstock_path):
@@ -109,11 +35,10 @@ def push_feedstock(feedstock_path):
         feedstock.fetch()               # git fetch origin
         feedstock.reset_hard()          # git reset --hard origin/master
 
-        # render the recipe to find the version
-        recipe_dir = os.path.join(feedstock_path, 'recipe')
-        recipes = render(recipe_dir, finalize=False)
-        metadata, download, needs_reparse = recipes[0]
-        version = metadata.version()
+        # find the recipe version
+        meta_filename = os.path.join(feedstock_path, 'recipe', 'meta.yaml')
+        recipe = CondaRecipe(meta_filename)
+        version = recipe.version
 
         # push to archive branch
         archive_branch = 'archive_{version}'.format(version=version)
