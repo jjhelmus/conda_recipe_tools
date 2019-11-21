0568c49a2449df68772931b841ae866e9f49203a
Jonathan Helmus <jjhelmus@gmail.com>
2019-11-21 08:39:06 -0600

    pypi: determine package from cwd if not specified

--- conda_recipe_tools/cli/pypi.py
+++ conda_recipe_tools/cli/pypi.py
@@ -2,6 +2,8 @@
 # open PyPI to a
 
 import argparse
+import pathlib
+import sys
 import webbrowser
 
 PYPI_WITH_VERSION = 'https://pypi.python.org/pypi/{package}/{version}'
@@ -11,18 +13,33 @@ WAREHOUSE_WITH_VERSION = 'https://pypi.org/project/{package}/{version}'
 WAREHOUSE_NO_VERSION = 'https://pypi.org/project/{package}'
 
 
-def main():
+def get_pkg_name_from_cwd():
+    """ Return the package name if the cwd is a feedstock. """
+    path = pathlib.Path.cwd()
+    for part in reversed(path.parts):
+        if part.endswith('-feedstock'):
+            return part[:-10]
+    print("Error: cannot determine package name from cwd")
+    sys.exit(1)
 
+
+def main():
     parser = argparse.ArgumentParser(description='Open a PyPI package page')
     parser.add_argument(
-        'packages', type=str, nargs='+',
-        help='packages to open pages for, use package::version for versions')
+        'packages', type=str, nargs='*',
+        help=(
+            'packages to open pages for, use package::version for versions. '
+            'If no packages are specified, determine package name from cwd.'))
     parser.add_argument(
         '--warehouse', '-w', action='store_true',
         help='open in warehouse, pypi.org, default is PyPI, pypi.python.org')
     args = parser.parse_args()
 
-    for package in args.packages:
+    if len(args.packages) == 0:
+        packages = [get_pkg_name_from_cwd()]
+    else:
+        packages = args.packages
+    for package in packages:
         if '::' in package:
             package, version = package.split('::')
             if args.warehouse:
